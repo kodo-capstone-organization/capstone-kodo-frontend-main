3873d46259b0b56f5a24683e7d9ab415c1d57152
chandya <chandya.c99@gmail.com>
2021-08-31 21:10:26 +0800

    Update LayoutElements.js

--- src/components/LayoutElements.js
+++ src/components/LayoutElements.js
@@ -4,7 +4,7 @@ import {colours} from "../values/Colours";
 export const LayoutContentWrapper = styled.div`
   display: flex;
   flex-direction: row;
-  padding: 0 15rem;
+  padding: 0 10rem;
   background: ${colours.WHITE};
 `;
 
