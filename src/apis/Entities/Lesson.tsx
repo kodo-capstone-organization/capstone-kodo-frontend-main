d7195eb0789f1d3f86dcf07d7c76413c9621fbf9
elainety-l <elainet@comp.nus.edu.sg>
2021-09-03 09:21:57 +0800

    creation of lesson entity to populate progress page

---
+++ src/apis/Entities/Lesson.tsx
@@ -0,0 +1,10 @@
+import { Course } from "./Course";
+
+export interface Lesson {
+    lessonId: number
+    name: number,
+    description: string,
+    sequence: number,
+    // contents: Content[]
+    // completedLessons: Lesson[] // Todo
+}
