fe33aa88f5d1cd051870692a05d24baaa6563303
chandya <chandya.c99@gmail.com>
2021-09-05 21:41:58 +0800

    tag search not case sensitive anymore

--- src/pages/BrowseCourse/BrowseCoursePage/index.tsx
+++ src/pages/BrowseCourse/BrowseCoursePage/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link as RouterLink } from 'react-router-dom'
+import { Link as RouterLink } from "react-router-dom";
 import { Course } from "../../../apis/Entities/Course";
 import { getAllCourses } from "../../../apis/Course/CourseApis";
 
@@ -19,33 +19,32 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-import TextField from '@material-ui/core/TextField';
-import SearchIcon from '@material-ui/icons/Search';
-import ChipInput from 'material-ui-chip-input'
+import TextField from "@material-ui/core/TextField";
+import SearchIcon from "@material-ui/icons/Search";
+import ChipInput from "material-ui-chip-input";
 import { Chip } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      '& > *': {
+      "& > *": {
         margin: theme.spacing(1),
-        width: '25ch',
-      },
+        width: "25ch"
+      }
     },
     media: {
-      height: 170,
+      height: 170
     },
     chipInput: {
-      width: '100%',
+      width: "100%"
     }
-  }),
-  
+  })
 );
 
 function BrowseCourse() {
   const classes = useStyles();
   const [courses, setCourses] = useState<Course[]>();
-  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [searchTerm, setSearchTerm] = useState<string>("");
   const [tags, setTags] = useState<string[]>([]);
 
   useEffect(() => {
@@ -57,21 +56,22 @@ function BrowseCourse() {
 
   /** HELPER METHODS */
   const handleChipChange = (chips: any) => {
-      setTags(chips);
-  }
+    setTags(chips);
+  };
   console.log("tags", tags);
 
   const handleSearchTerm = (term: any) => {
     setSearchTerm(term);
-
-  }
+  };
 
   //Return an array of Courses that match the tags entered by user
   function returnTagMatch(val: Course): boolean {
-      let courseAllTags = val.courseTags;
-      var result = courseAllTags.map(function(a) {return a.title;});
-      console.log(tags.every(t => result.includes(t)))
-      return tags.every(t => result.includes(t));
+    let courseAllTags = val.courseTags;
+    var result = courseAllTags.map(function(a) {
+      return a.title.toLowerCase();
+    });
+    console.log(tags.every(t => result.includes(t.toLowerCase())));
+    return tags.every(t => result.includes(t.toLowerCase()));
   }
 
   /** 
@@ -80,56 +80,70 @@ function BrowseCourse() {
   }
   */
 
-
   return (
     //This would encompass the whole container for component
     <>
-    <BrowseContainer>
-      <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="Search By Name" variant="outlined" onChange={event => {setSearchTerm(event.target.value)}}/>
-        <ChipInput 
-        label="Search By Tags"
-        onChange={(chips) => handleChipChange(chips)}
-        />
-      </form>
-      <Title>Courses</Title>
-      <CourseWrapper>
-        {courses?.filter((val) => {
-          if (searchTerm == "" && tags.length == 0) {
-            return val;
-          } else if (tags.length > 0 && returnTagMatch(val)){
-            return val;
-          } else if (searchTerm !== "" && val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-            return val;
-          } 
-        }).map(course => {
-          return (
-            <>
-              <CourseCard key={course.courseId}>
-                <CardActionArea component={RouterLink} to={`/browsecourse/preview/${course.courseId}`}>
-                  <CardMedia
-                    className={classes.media}
-                    image="/chessplaceholder.png"
-                    title={course.name}
-                  />
-                  <CourseCardContent>
-                    <Typography>{course.name}</Typography>
-                    <Typography
-                      variant="body2"
-                      color="textSecondary" 
-                      component="p"
+      <BrowseContainer>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField
+            id="outlined-basic"
+            label="Search By Name"
+            variant="outlined"
+            onChange={event => {
+              setSearchTerm(event.target.value);
+            }}
+          />
+          <ChipInput
+            label="Search By Tags"
+            onChange={chips => handleChipChange(chips)}
+          />
+        </form>
+        <Title>Courses</Title>
+        <CourseWrapper>
+          {courses
+            ?.filter(val => {
+              if (searchTerm == "" && tags.length == 0) {
+                return val;
+              } else if (tags.length > 0 && returnTagMatch(val)) {
+                return val;
+              } else if (
+                searchTerm !== "" &&
+                val.name.toLowerCase().includes(searchTerm.toLowerCase())
+              ) {
+                return val;
+              }
+            })
+            .map(course => {
+              return (
+                <>
+                  <CourseCard key={course.courseId}>
+                    <CardActionArea
+                      component={RouterLink}
+                      to={`/browsecourse/preview/${course.courseId}`}
                     >
-                      {course.tutor.name}
-                    </Typography>
-                  </CourseCardContent>
-                </CardActionArea>
-              </CourseCard>
-            </>
-          );
-        })}
-      </CourseWrapper>
-      <Title>Suggested For You [To Do]</Title>
-    </BrowseContainer>
+                      <CardMedia
+                        className={classes.media}
+                        image="/chessplaceholder.png"
+                        title={course.name}
+                      />
+                      <CourseCardContent>
+                        <Typography>{course.name}</Typography>
+                        <Typography
+                          variant="body2"
+                          color="textSecondary"
+                          component="p"
+                        >
+                          {course.tutor.name}
+                        </Typography>
+                      </CourseCardContent>
+                    </CardActionArea>
+                  </CourseCard>
+                </>
+              );
+            })}
+        </CourseWrapper>
+        <Title>Suggested For You [To Do]</Title>
+      </BrowseContainer>
     </>
   );
 }
