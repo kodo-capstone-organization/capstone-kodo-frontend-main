d1f53854236c263f8ccef10e64e668f97e107183
chandya <chandya.c99@gmail.com>
2021-09-06 03:29:42 +0800

    sidebar in progress

--- src/pages/CourseViewer/Sidebar/Sidebar.tsx
+++ src/pages/CourseViewer/Sidebar/Sidebar.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { Course } from "../../../apis/Entities/Course";
 
-function Sidebar() {
+function Sidebar(props: any) {
+    const [currentCourse, setCourse] = useState<Course>({...props.course});
+
+    useEffect(() => {
+        setCourse(props.course)
+    }, [props.course])
+
+    let allLessons = currentCourse?.lessons;
+
     return (
         <div>
-            
+            <h3>sidebar for: {currentCourse?.name}</h3>
+            {
+                allLessons?.map((lesson) => {
+                    return (
+                        <div key={lesson.lessonId}>
+                            <p>
+                                {lesson.name}
+                            </p>
+                        </div>
+                    );
+                })
+            }
         </div>
     )
 }
--- src/pages/CourseViewer/index.tsx
+++ src/pages/CourseViewer/index.tsx
@@ -1,37 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { withRouter } from "react-router";
-import { Course } from "../../apis/Entities/Course";
-import { Account } from "../../apis/Entities/Account";
+import React, { useEffect, useState } from "react";
+import { withRouter } from "react-router";
+import { Course } from "../../apis/Entities/Course";
+import { Account } from "../../apis/Entities/Account";
 import { getMyAccount } from "../../apis/Account/AccountApis";
-import { getCourseByCourseId } from "../../apis/Course/CourseApis";
+import { getCourseByCourseId } from "../../apis/Course/CourseApis";
+import Sidebar from "./Sidebar/Sidebar";
 
-function CourseOverview(props: any)  {
-    const courseId = props.match.params.courseId;
-    const [currentUser, setUser] = useState<Account>();
-    const [currentCourse, setCourse] = useState<Course>();
-    const accountId = JSON.parse(window.sessionStorage.getItem('loggedInAccountId') || '{}');
+function CourseOverview(props: any) {
+  const courseId = props.match.params.courseId;
+  const [currentUser, setUser] = useState<Account>();
+  const [currentCourse, setCourse] = useState<Course>();
+  const accountId = JSON.parse(
+    window.sessionStorage.getItem("loggedInAccountId") || "{}"
+  );
 
-    useEffect(() => {
-        getCourseByCourseId(courseId).then(receivedCourse => {
-            setCourse(receivedCourse);
-            console.log(receivedCourse.name);
-        });
-    }, []);
-        
-    useEffect(() => {
-        getMyAccount(accountId).then(receivedAccount => {
-            setUser(receivedAccount)
-        });
-    }, []);
+  useEffect(() => {
+    getCourseByCourseId(courseId).then(receivedCourse => {
+      setCourse(receivedCourse);
+      console.log(receivedCourse.name);
+    });
+  }, []);
 
+  useEffect(() => {
+    getMyAccount(accountId).then(receivedAccount => {
+      setUser(receivedAccount);
+    });
+  }, []);
 
-    return (
-        <div>
-            <h1>Course overview of {currentCourse?.name}</h1>
-        </div>
-    )
+  const allLessons = currentCourse?.lessons;
+  console.log(allLessons);
+
+  return (
+    <div>
+        <Sidebar course={currentCourse}/>
+        <h1>Course overview of {currentCourse?.name}</h1>
+    </div>
+  );
 }
 
 const CourseOverviewWithRouter = withRouter(CourseOverview);
 export default CourseOverviewWithRouter;
-
