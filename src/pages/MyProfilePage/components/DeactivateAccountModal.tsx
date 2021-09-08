fee88720d26617e7f0aa16f9c0555663dca3abca
elainety-l <elainet@comp.nus.edu.sg>
2021-09-08 11:08:06 +0800

    designing assets for deactivate

--- src/pages/MyProfilePage/components/DeactivateAccountModal.tsx
+++ src/pages/MyProfilePage/components/DeactivateAccountModal.tsx
@@ -1,19 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import { Button } from "../../../values/ButtonElements";
 import { Account } from "../../../apis/Entities/Account";
 import { TextSpan } from "../ProfileElements";
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { Chip } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -67,7 +64,7 @@ function DeactivateAccountModal(props: any) {
     return (
         <>
             <div>
-                <Button big={false} onClick={handleOpen}>Deactivate/Reactivate Account</Button>
+                <Button big style={{width:"fit-content", margin:"20px auto 10px auto"}} onClick={handleOpen}>{isActive ? "Deactivate" : "Activate"} Account</Button>
                 <Dialog open={open} onClose={handleClose}>
                     <div style={{ display: "flex" }}>
                         <DialogTitle id="form-dialog-title">Deactivate/Reactivate Your Kodo Account</DialogTitle>
@@ -77,15 +74,12 @@ function DeactivateAccountModal(props: any) {
                     </div>
                     <DialogContent>
                         <DialogContentText>
-                            You are currently <TextSpan isActive={isActive}>{isActive ? "activated" : "deactivated"}.</TextSpan>
+                            Account Status: <Chip variant="outlined" label={isActive ? "Activated" : "Deactivated"} style={{ color: isActive ? "green" : "red", border: isActive ? "1px solid green" : "1px solid red" }} />
                         </DialogContentText>
                     </DialogContent>
                     <DialogActions>
-                        <Button onClick={handleClose} color="primary">
+                        <Button onClick={handleAction} primary>
                             {isActive ? "Deactivate" : "Activate"} My Account
-                        </Button>
-                        <Button onClick={handleClose} color="primary">
-                            Cancel
                         </Button>
                     </DialogActions>
                 </Dialog>
