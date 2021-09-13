73c9c743de46dcdb63c828fc63a414cf5707d778
FluffDucks <fluffducks@outlook.com>
2021-09-14 02:54:09 +0800

    Implement Blank State for Lesson Builder

--- src/pages/CourseBuilderPage/components/LessonPlan.tsx
+++ src/pages/CourseBuilderPage/components/LessonPlan.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { Lesson } from './../../../apis/Entities/Lesson';
-import { AppBar, Tabs, Tab, Grid, IconButton, TextField } from "@material-ui/core";
+import { AppBar, Tabs, Tab, Grid, IconButton, TextField, Typography } from "@material-ui/core";
 import AddIcon from '@material-ui/icons/Add';
 import { CourseBuilderCardHeader, CourseBuilderContent } from "./../CourseBuilderElements";
 import { tabProps, TabPanel }from './TabPanel';
 import QuizTable from './QuizTable';
 import MultimediaTable from './MultimediaTable';
 import { Button } from "../../../values/ButtonElements";
+import { BlankStateContainer } from '../../MyProfilePage/ProfileElements';
 
 function LessonPlan(props: any) {
     const handleFormDataChange = props.handleFormDataChange;
@@ -72,60 +73,74 @@ function LessonPlan(props: any) {
     return (
         <>
         <CourseBuilderCardHeader
-                    title="Lesson Plan"
-                    action={
-                        <IconButton color="primary" onClick={addToLessons}>
-                            <AddIcon/>&nbsp; Add Lesson
-                        </IconButton>
-                    }/>
-            <AppBar position="static" color="default">
-                <Tabs value={tabValue}
-                    onChange={handleTabChange}
-                    indicatorColor="primary"
-                    textColor="primary"
-                    variant="scrollable"
-                    scrollButtons="auto"
-                    aria-label="scrollable auto tabs example">
-                        {lessons?.map((lesson, index) => {
-                            return (<Tab label={"Lesson " + index} {...tabProps(index)}/>)
-                        })}
-                </Tabs>
-            </AppBar>
-            {lessons?.map((lesson, index) => {
-                return (
-                    <TabPanel value={tabValue} index={index}>
-                        <CourseBuilderContent key={index}>
-                            <Grid container spacing={3}>
-                                <Grid style={{ padding: "0!important"}} item xs={12}>
-                                    <TextField fullWidth required
-                                    id={index.toString()}
-                                    label="Name"
-                                    value={lesson.name}
-                                    onChange={handleLessonNameChange}/>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <QuizTable
-                                    handleFormDataChange={handleFormDataChange}
-                                    lessonIndex={index}
-                                    quizzes={lesson.quizzes}
-                                    lessons={lessons}/>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <MultimediaTable
-                                    handleFormDataChange={handleFormDataChange}
-                                    lessonIndex={index}
-                                    multimedias={lesson.multimedias}
-                                    lessons={lessons}/>
-                                </Grid>
-                                <Grid container xs={12} justify="flex-end">
-                                    <Button onClick={() => handleDeleteLesson(lesson.lessonId)}>
-                                        Delete Lesson
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </CourseBuilderContent>
-                    </TabPanel>
-            )})}
+            title="Lesson Plan"
+            action={
+                <IconButton color="primary" onClick={addToLessons}>
+                    <AddIcon/>&nbsp; Add Lesson
+                </IconButton>
+            }/>
+            {lessons.length === 0 &&
+                <BlankStateContainer style={{ padding: "2rem"}}>
+                    <Typography variant="h5">Begin building your course by adding one or more lessons! 🤓</Typography>
+                    <br/>
+                    <Button style={{width: "10%" }} onClick={addToLessons} big>Add Lesson</Button>
+                </BlankStateContainer>
+            }
+            {lessons.length > 0 &&
+                <>
+                    <AppBar position="static" color="default">
+                        <Tabs value={tabValue}
+                              onChange={handleTabChange}
+                              indicatorColor="primary"
+                              textColor="primary"
+                              variant="scrollable"
+                              scrollButtons="auto"
+                              aria-label="scrollable auto tabs example">
+                            {lessons?.map((lesson, index) => {
+                                return (<Tab label={"Lesson " + index} {...tabProps(index)}/>)
+                            })}
+                        </Tabs>
+                    </AppBar>
+                    { lessons?.map((lesson, index) => {
+                        return (
+                            <TabPanel value={tabValue} index={index}>
+                                <CourseBuilderContent key={index}>
+                                    <Grid container spacing={3}>
+                                        <Grid style={{ padding: "0!important"}} item xs={12}>
+                                            <TextField fullWidth required
+                                                       id={index.toString()}
+                                                       label="Name"
+                                                       value={lesson.name}
+                                                       onChange={handleLessonNameChange}/>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <QuizTable
+                                                handleFormDataChange={handleFormDataChange}
+                                                lessonIndex={index}
+                                                quizzes={lesson.quizzes}
+                                                lessons={lessons}
+                                            />
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <MultimediaTable
+                                                handleFormDataChange={handleFormDataChange}
+                                                lessonIndex={index}
+                                                multimedias={lesson.multimedias}
+                                                lessons={lessons}
+                                            />
+                                        </Grid>
+                                        <Grid container xs={12} justify="flex-end">
+                                            <Button onClick={() => handleDeleteLesson(lesson.lessonId)}>
+                                                Delete Lesson
+                                            </Button>
+                                        </Grid>
+                                    </Grid>
+                                </CourseBuilderContent>
+                            </TabPanel>
+                        )}
+                    )}
+                </>
+            }
         </>
     )
 }
