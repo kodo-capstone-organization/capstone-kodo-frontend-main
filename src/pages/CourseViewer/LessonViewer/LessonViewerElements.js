58c0578049539a4b31dbeec9ca6f81715d0c6d11
chandya <chandya.c99@gmail.com>
2021-09-28 02:22:33 +0800

    removed link disabling for testing purposes for ayush

--- src/pages/CourseViewer/LessonViewer/LessonViewerElements.js
+++ src/pages/CourseViewer/LessonViewer/LessonViewerElements.js
@@ -83,8 +83,7 @@ export const ContentLink = styled(Link)`
   transition: 0.2s ease-in-out;
   color: ${colours.GRAY5};
   cursor: pointer;
-  pointer-events: ${({ isCompleted, previousCompleted }) =>
-    (isCompleted || !previousCompleted) ? "none" : "auto"};
+  pointer-events: auto;
   font-family: "Roboto", sans-serif;
   padding: 0 20px 0 20px;
 
--- src/pages/CourseViewer/LessonViewer/index.tsx
+++ src/pages/CourseViewer/LessonViewer/index.tsx
@@ -173,16 +173,11 @@ function LessonViewer(props: any) {
             {lessonMultimedias?.map(m => {
               return (
                 <ContentLink
-                  key={m.contentId}
-                  isCompleted={checkCompleted(m.contentId)}
-                  previousCompleted={previousLessonCompleted()}
+                  key={m.contentId} 
                   to={`/overview/lesson/${courseId}/${lessonId}/${m.contentId}`}
                 >
                   {m.multimediaType === "PDF" ? <ReadingIcon /> : <PlayIcon />}
                   {m.multimediaType === "PDF" ? "Reading" : "Video"}: {m.name}
-                  {checkCompleted(m.contentId) &&
-                  <CheckIcon />
-                  }
                 </ContentLink>
               );
             })}
@@ -190,7 +185,6 @@ function LessonViewer(props: any) {
         </LessonCard>
         <LessonCard>
           <LessonHeader>Quiz</LessonHeader>
-          <QuizHeading>{}</QuizHeading>
           <QuizWrapper>
             {getQuizAttempts()?.map(q => {
               return (
