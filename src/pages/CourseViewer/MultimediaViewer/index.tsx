9fe637e1110d03bd142ad4160adb66cadb992783
AyushK98 <65055840+AyushK98@users.noreply.github.com>
2021-09-27 22:04:23 +0800

    Multimedia Viewer (IP)

--- src/pages/CourseViewer/MultimediaViewer/index.tsx
+++ src/pages/CourseViewer/MultimediaViewer/index.tsx
@@ -1,21 +1,123 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router";
+import { getCourseByCourseId } from "../../../apis/Course/CourseApis";
+import { getLessonByLessonId } from "../../../apis/Lesson/LessonApis";
+import { getMyAccount } from "../../../apis/Account/AccountApis";
+import { getEnrolledLesson } from "../../../apis/EnrolledLesson/EnrolledLessonApis";
+import { getEnrolledCourseByStudentIdAndCourseId } from "../../../apis/EnrolledCourse/EnrolledCourseApis";
 import { getMultimediaByMultimediaId } from "../../../apis/Multimedia/MultimediaApis";
 import { Multimedia } from "../../../apis/Entities/Multimedia";
+import { Course } from "../../../apis/Entities/Course";
+import { Lesson } from "../../../apis/Entities/Lesson";
+import { Account } from "../../../apis/Entities/Account";
+import { Quiz } from "../../../apis/Entities/Quiz";
+import { EnrolledLesson } from "../../../apis/Entities/EnrolledLesson";
+import { EnrolledCourse } from "../../../apis/Entities/EnrolledCourse";
+import { QuizWithStudentAttemptCountResp } from "../../../apis/Entities/Quiz"
 
+import { Button } from "../../../values/ButtonElements";
+import { colours } from "../../../values/Colours";
+import ReactPlayer from "react-player";
+import { Document, Page, pdfjs } from "react-pdf";
+
+import {
+  MultimediaContainer,
+  PageHeadingAndButton,
+  PageHeading,
+  LessonTitle,
+  CourseTitle,
+  MultimediaCard,
+  VideoCard,
+  PDFCard,
+  ImageCard,
+  MultimediaName,
+  MultimediaDescription,
+  MultimediaHeader,
+  ExitWrapper
+} from "./MultimediaViewerElements";
+
+import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function MultimediaViewer(props: any) {
   const contentId = props.match.params.contentId;
+  const lessonId = props.match.params.lessonId;
+  const courseId = props.match.params.courseId;
+
   const [currentMultimedia, setMultimedia] = useState<Multimedia>();
+  const [currentLesson, setLesson] = useState<Lesson>();
+  const [currentCourse, setCourse] = useState<Course>();
 
   useEffect(() => {
     getMultimediaByMultimediaId(contentId).then(receivedMultimedia => {
       setMultimedia(receivedMultimedia);
-    }); 
+    });
+    getLessonByLessonId(lessonId).then(receivedLesson => {
+      setLesson(receivedLesson);
+    });
+    getCourseByCourseId(courseId).then(receivedCourse => {
+      setCourse(receivedCourse);
+    });
   }, []);
 
-  return (<div>hello! {currentMultimedia?.multimediaType} goes here</div>)
+  // return (<div>hello! {currentMultimedia?.multimediaType} goes here</div>)
+  return (
+    <>
+      <MultimediaContainer>
+        <PageHeadingAndButton>
+          <PageHeading>
+            <LessonTitle>Week {currentLesson?.sequence}</LessonTitle>
+            <CourseTitle>{currentCourse?.name}</CourseTitle>
+          </PageHeading>
+          <ExitWrapper to={`/overview/${currentCourse?.courseId}`}>
+            <CancelOutlinedIcon fontSize="large" style={{ color: colours.BLUE2, padding: 20 }} />
+          </ExitWrapper>
+        </PageHeadingAndButton>
+        <MultimediaCard>
+          <MultimediaHeader>Multimedia Overview</MultimediaHeader>
+          <MultimediaName> Name: {currentMultimedia?.name}</MultimediaName>
+          <MultimediaDescription> Description: {currentMultimedia?.description}</MultimediaDescription>
+        </MultimediaCard>
+        <VideoCard>
+          {currentMultimedia?.multimediaType === "VIDEO" &&
+          <ReactPlayer 
+            url = {currentMultimedia.url}
+            controls = {true}
+          />
+          }
+        </VideoCard>
+
+        {/* <ImageCard>
+          {currentMultimedia?.multimediaType === "IMAGE" &&
+          // <ReactPlayer 
+          //   url = {currentMultimedia.url}
+          //   controls = {true}
+          // />
+          <iframe width="100%" height="600" src={`https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik`}></iframe>
+
+          }
+        </ImageCard> */}
+
+        <PDFCard>
+          {currentMultimedia?.multimediaType === "PDF" &&
+          <Document file= "https://cors-anywhere.herokuapp.com/" + {currentMultimedia.url} />
+          }
+          {/* <Document file= {"./dummy.pdf"} />
+          {currentMultimedia?.url} */}
+          {/* <Document
+            file={{
+              url:
+                "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+            }}
+            // onLoadSuccess={onDocumentLoadSuccess}
+          ></Document> */}
+        </PDFCard>
+
+      </MultimediaContainer>
+    </>
+  );
 }
 
 const MultimediaViewerWithRouter = withRouter(MultimediaViewer);
 export default MultimediaViewerWithRouter;
+
