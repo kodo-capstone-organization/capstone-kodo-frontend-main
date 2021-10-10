2e93d9c22c02e65120961e1418a481265a63ddc1
FluffDucks <fluffducks@outlook.com>
2021-10-11 04:24:33 +0800

    Create rough data channel message event structure

--- src/apis/Entities/Session.tsx
+++ src/apis/Entities/Session.tsx
@@ -11,3 +11,34 @@ export interface InvitedSessionResp {
     sessionId: string,
     hostId: number
 }
+
+export interface KodoDataChannelMessage {
+    peerId: number,
+    eventType: KodoSessionEventType,
+    event: KodoSessionEvent
+}
+
+export interface KodoSessionEvent {
+    eventType: KodoSessionEventType
+}
+
+export enum KodoSessionEventType {
+    CALL = "CALL",
+    WHITEBOARD = "WHITEBOARD",
+    EDITOR = "EDITOR"
+}
+
+export interface CallEvent extends KodoSessionEvent {
+    message?: string,
+    isMuted?: boolean
+}
+
+export interface WhiteboardEvent extends KodoSessionEvent {
+    eventType: KodoSessionEventType.WHITEBOARD
+    // TODO
+}
+
+export interface EditorEvent extends KodoSessionEvent {
+    eventType: KodoSessionEventType.EDITOR,
+    // TODO
+}
