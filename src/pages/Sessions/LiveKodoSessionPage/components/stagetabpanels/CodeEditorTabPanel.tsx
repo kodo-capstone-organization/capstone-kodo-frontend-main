bfbbb5d73a25d7aa4371f906414b235c136de06c
FluffDucks <fluffducks@outlook.com>
2021-10-27 03:32:56 +0800

    Implement basic monaco integration

--- src/pages/Sessions/LiveKodoSessionPage/components/stagetabpanels/CodeEditorTabPanel.tsx
+++ src/pages/Sessions/LiveKodoSessionPage/components/stagetabpanels/CodeEditorTabPanel.tsx
@@ -1,48 +1,64 @@
-import {Grid, Menu, MenuItem } from "@material-ui/core";
+import MonacoEditor, { monaco } from 'react-monaco-editor';
+import { FormControl, InputLabel, Menu, MenuItem, Select, Typography } from "@material-ui/core";
 import { useState } from "react";
-import { CodeEditorPanelWrapper, LanguageSelectorButton } from "./StageTabPanelsElements";
+import { CodeEditorPanelWrapper, EditorTopBarGrid } from "./StageTabPanelsElements";
+
+// Monaco settings
+const options = {
+    selectOnLineNumbers: true,
+    fontSize: 20
+}
 
 function CodeEditorTabPanel (props: any) {
 
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const [selectedLanguage, setSelectedLanguage] = useState<string>("Python");
+    const [selectedLanguage, setSelectedLanguage] = useState<string>("python");
+    const [isEditorLoading, setIsEditorLoading] = useState<boolean>(true);
+    const [editorCode, setEditorCode] = useState<string>("");
 
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = (language: string) => {
-        setSelectedLanguage(language);
+    const handleLanguageChange = (event: any) => {
+        setSelectedLanguage(event?.target?.value as string);
         // TODO send dc message to everyone that language is changed
-        setAnchorEl(null);
     };
+
+    const editorDidMount = () => {
+        // TODO any pre-setup
+        setIsEditorLoading(false);
+    }
+
+    const onCodeChange = (newCodeValue: string, event: monaco.editor.IModelContentChangedEvent) => {
+        // TODO fire datachannel message
+        setEditorCode(newCodeValue)
+    }
 
     return (
         <CodeEditorPanelWrapper>
-            <Grid container style={{ justifyContent: "flex-end", padding: "0.5rem" }}>
-                <LanguageSelectorButton
-                    aria-controls="language-menu"
-                    variant="extended"
-                    size="medium"
-                    color="primary"
-                    aria-label="select-language"
-                    aria-haspopup="true"
-                    onClick={handleClick}
-                >
-                    {selectedLanguage}
-                </LanguageSelectorButton>
-                <Menu
-                    id="language-menu"
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={handleClose}
-                >
-                    <MenuItem onClick={() => handleClose("Python")}>Python</MenuItem>
-                    <MenuItem onClick={() => handleClose("Java")}>Java</MenuItem>
-                    <MenuItem onClick={() => handleClose("Javascript")}>Javascript</MenuItem>
-                </Menu>
-            </Grid>
+            <EditorTopBarGrid container>
+                <Typography variant="h6">
+                    Placeholder Code File Name
+                </Typography>
+                <FormControl variant="outlined" margin="dense" color="primary" style={{ minWidth: "120px"}}>
+                    <InputLabel id="select-language-label">Language</InputLabel>
+                    <Select
+                        labelId="select-language-label"
+                        id="select-language"
+                        value={selectedLanguage}
+                        onChange={handleLanguageChange}
+                        label="Language"
+                    >
+                        <MenuItem value="python">Python</MenuItem>
+                        <MenuItem value="java">Java</MenuItem>
+                        <MenuItem value="javascript">Javascript</MenuItem>
+                    </Select>
+                </FormControl>
+            </EditorTopBarGrid>
+            <MonacoEditor
+                language={selectedLanguage}
+                theme="vs-light"
+                value={editorCode}
+                options={options}
+                onChange={onCodeChange}
+                editorDidMount={editorDidMount}
+            />
         </CodeEditorPanelWrapper>
     )
 }
