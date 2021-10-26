069023e84819c97eae20519076dd229460d6d524
FluffDucks <fluffducks@outlook.com>
2021-10-26 14:02:37 +0800

    Implement language select button

--- src/pages/Sessions/LiveKodoSessionPage/components/stagetabpanels/StageTabPanelsElements.js
+++ src/pages/Sessions/LiveKodoSessionPage/components/stagetabpanels/StageTabPanelsElements.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { fontSizes } from "../../../../../values/FontSizes";
 import { colours } from "../../../../../values/Colours";
+import { Fab } from "@material-ui/core";
 
 export const DebugInfoPanelWrapper = styled.div`
     padding: 1.5rem;
@@ -12,4 +13,15 @@ export const WhiteboardPanelWrapper = styled.div`
     align-items: center;
     height: inherit;
     width: inherit;
+`;
+
+export const CodeEditorPanelWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: inherit;
+    width: inherit;
+`;
+
+export const LanguageSelectorButton = styled(Fab)`
 `;
